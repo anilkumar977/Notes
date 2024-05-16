7b8b76ccc35f0042072800b489a406d5cad9a469
anilkumar977 <anilkumabr30@gmail.com>
2024-05-16 21:00:16 +0530

    done

--- src/main/java/com/org/dao/UserDao.java
+++ src/main/java/com/org/dao/UserDao.java
@@ -12,49 +12,46 @@ import com.org.dto.User;
 
 public class UserDao {
 
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("Anil");
+	
+	EntityManager em = emf.createEntityManager();
+	
+	EntityTransaction et = em.getTransaction();
+	
 			public void saveAndUpdateUser(User user) {
 				
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("Anil");
-			
-			EntityManager em = emf.createEntityManager();
-			
-			EntityTransaction et = em.getTransaction();
-			
-			et.begin();
-			em.merge(user);
-			et.commit();
-			
+				et.begin();
+				em.merge(user);
+				et.commit();
 			
 		}
 		
 		public void fetchUserById(int id){
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("Anil");
 			
-			EntityManager em = emf.createEntityManager();
 			
 			User user = em.find(User.class, id);
 			
 		}
 		
-		public List <User> fetchAllUser() {
+		public User fetchUserByEmailAndPassword(String email,String password) {
 			
-			EntityManagerFactory emf = Persistence.createEntityManagerFactory("Anil");
 			
-			EntityManager em = emf.createEntityManager();
 			
-			Query query = em.createQuery("select s from User s");
+			Query query = em.createQuery("select s from User s where s.email=?1 and s.password=?2");
+				
+				query.setParameter(1, email);
+				query.setParameter(2, password);
 			
 			List <User> users = query.getResultList();
-			
-			for(User u : users) {
-				
-				u.getId();
-				u.getName();
-				u.getAge();
-				u.getEmail();
-				u.getPassword();
+			if(users.isEmpty())
+				return null;
+//			for(User u : users) {
+//			
+//				u.getId();
+//				u.getName();
+//				u.getAge();
+//				u.getEmail();
+//				u.getPassword();
+			return users.get(0);
 			}
-			return users;
 		}
-
-	}
